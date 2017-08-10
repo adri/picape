541cb642e119f52ff7a2ff66ad816508cdc6fa41
Adrian Philipp <adrian@ticketswap.com>
2017-08-10 22:13:08 +0200

    Invalidate supermarket cart cache on refresh

--- lib/picape_web/graphql/resolver/order.ex
+++ lib/picape_web/graphql/resolver/order.ex
@@ -1,6 +1,10 @@
 defmodule PicapeWeb.Graphql.Resolver.Order do
 
-  alias Picape.Order
+  alias Picape.{Supermarket, Order}
+
+  defp order_id() do
+    1
+  end
 
 # Queries
 
@@ -9,19 +13,19 @@ defmodule PicapeWeb.Graphql.Resolver.Order do
   end
 
   def recipies_planned?(_, recipe_ids) do
-    {:ok, planned_ids} = Order.planned_recipes(1)
+    {:ok, planned_ids} = Order.planned_recipes(order_id())
 
     Map.new(recipe_ids, fn id -> {id, Enum.member?(planned_ids, id)} end)
   end
 
   def ingredients_planned?(_, ingredient_ids) do
-    {:ok, ingredients} = Order.ingredients_planned?(1, ingredient_ids)
+    {:ok, ingredients} = Order.ingredients_planned?(order_id(), ingredient_ids)
 
     ingredients
   end
 
   def ingredients_ordered_quantity(_, ingredient_ids) do
-    {:ok, ingredients} = Order.ingredients_ordered_quantity(1, ingredient_ids)
+    {:ok, ingredients} = Order.ingredients_ordered_quantity(order_id(), ingredient_ids)
 
     ingredients
   end
@@ -29,18 +33,19 @@ defmodule PicapeWeb.Graphql.Resolver.Order do
 # Mutations
 
   def plan_recipe(attributes, _info) do
-    Order.plan_recipe(1, attributes[:recipe_id], false)
+    Order.plan_recipe(order_id(), attributes[:recipe_id], false)
   end
 
   def unplan_recipe(attributes, _info) do
-    Order.plan_recipe(1, attributes[:recipe_id], true)
+    Order.plan_recipe(order_id(), attributes[:recipe_id], true)
   end
 
   def sync_supermarket(attributes, _info) do
-    Order.sync_supermarket(1)
+    Supermarket.invalidate_cart()
+    Order.sync_supermarket(order_id())
   end
 
   def order_ingredient(attributes, _info) do
-    Order.order_ingredient(1, attributes[:ingredient_id], attributes[:quantity])
+    Order.order_ingredient(order_id(), attributes[:ingredient_id], attributes[:quantity])
   end
 end
