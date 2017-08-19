19eb5ff41d399c5aab98910265c32a4695dffb29
Adrian Philipp <adrian@ticketswap.com>
2017-08-19 14:43:19 +0200

    Animate nav

--- assets/components/Nav.js
+++ assets/components/Nav.js
@@ -9,13 +9,13 @@ export default () => (
           <span className="navbar-toggler-bar bar2"/>
           <span className="navbar-toggler-bar bar3"/>
         </button>
-        <Link className="navbar-brand" href="/"><a>Picape</a></Link>
+        <Link href="/"><a className="logo">Picape</a></Link>
       </div>
       <div className="collapse navbar-collapse justify-content-end" id="navigation" data-nav-image="./assets/img/blurred-image-1.jpg">
         <ul className="navbar-nav">
           <li className="nav-item">
             <Link href="/ingredients">
-              <a className="nav-link">
+              <a className="nav-link link">
                 <i className="now-ui-icons files_paper" />
                 <p>Ingredients</p>
               </a>
@@ -23,7 +23,7 @@ export default () => (
           </li>
           <li className="nav-item">
             <Link href="/recipes">
-              <a className="nav-link">
+              <a className="nav-link link">
                 <i className="now-ui-icons files_paper" />
                 <p>Recipes</p>
               </a>
@@ -32,5 +32,31 @@ export default () => (
         </ul>
       </div>
     </div>
+    <style jsx>{`
+    .logo{
+        font-size: 25px;
+        transition: all 0.4s ease;
+    }
+
+    .logo:hover {
+        text-decoration: none;
+        transition: all 0.4s ease;
+        transform: scaleY(-1);
+    }
+
+    .nav-link {
+        opacity: 0.8;
+        transition: opacity 0.3s;
+    }
+
+    .nav-link p {
+        font-size: 13px;
+    }
+
+    .nav-link:hover {
+        opacity: 1.0;
+        transition: opacity 0.3s;
+    }
+    `}</style>
   </nav>
 )
