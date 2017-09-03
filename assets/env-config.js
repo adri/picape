37f3478adf64346b4013bfedf6eccbff88136c8b
Adrian Philipp <adrian@ticketswap.com>
2017-09-03 16:53:31 +0200

    Adds raven js

--- assets/env-config.js
+++ assets/env-config.js
@@ -1,5 +1,6 @@
 const prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  'BACKEND_URL': prod ? 'https://' + process.env.HOST : 'http://localhost:4000'
+  'BACKEND_URL': prod ? 'https://' + process.env.HOST : 'http://localhost:4000',
+  'SENTRY_PUBLIC_DSN': prod ? process.env.SENTRY_PUBLIC_DSN: 'https://xxxx@sentry.io/xxxx',
 };
