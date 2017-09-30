b1afd7ef94f95c1f847929a83b129a31f4a33c2d
Adrian Philipp <adrian@ticketswap.com>
2017-09-30 11:53:42 +0200

    Cleanup absinthe test case

--- test/support/absinthe_case.ex
+++ test/support/absinthe_case.ex
@@ -4,18 +4,9 @@ defmodule Picape.AbsintheCase do
   using do
     quote do
       use ExUnit.Case
-      ExUnit.Case.register_attribute __ENV__, :query
-      ExUnit.Case.register_attribute __ENV__, :variables
-
       use Picape.DataCase, async: true
       import Picape.AbsintheCase
     end
-  end
-
-  setup %{registered: reg} = ctx do
-    ctx = if reg[:query], do: Map.put(ctx, :query, reg[:query]), else: ctx
-    ctx = if reg[:variables], do: Map.put(ctx, :variables, reg[:variables]), else: ctx
-    {:ok, ctx}
   end
 
   def run(document, options \\ [], schema \\ PicapeWeb.Graphql.Schema) do
