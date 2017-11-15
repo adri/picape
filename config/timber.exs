369f11e2f9e8fe8b43f371e76e30523f96190020
Adrian Philipp <adrian@ticketswap.com>
2017-11-15 12:49:32 +0100

    Disable timber in dev mode
    Really annoying that it replaces the default logger which contains better
    logging (sql data) and colors.

--- config/timber.exs
+++ config/timber.exs
@@ -1,23 +1,5 @@
 use Mix.Config
 
-# Update the instrumenters so that we can structure Phoenix logs
-config :picape, PicapeWeb.Endpoint,
-  instrumenters: [Timber.Integrations.PhoenixInstrumenter]
-
-# Structure Ecto logs
-config :picape, Picape.Repo,
-  loggers: [{Timber.Integrations.EctoLogger, :log, []}]
-
-# Use Timber as the logger backend
-# Feel free to add additional backends if you want to send you logs to multiple devices.
-# Deliver logs via HTTP to the Timber API by using the Timber HTTP backend.
-config :logger,
-  backends: [Timber.LoggerBackends.HTTP],
-  utc_log: true
-
-config :timber,
-  api_key: {:system, "TIMBER_LOGS_KEY"}
-
 # For the following environments, do not log to the Timber service. Instead, log to STDOUT
 # and format the logs properly so they are human readable.
 environments_to_exclude = [:test, :dev]
@@ -25,18 +7,36 @@ if Enum.member?(environments_to_exclude, Mix.env()) do
   # Fall back to the default `:console` backend with the Timber custom formatter
   config :logger,
     backends: [:console],
-    utc_log: true
+    utc_log: false
 
-  config :logger, :console,
-    format: {Timber.Formatter, :format},
-    metadata: [:timber_context, :event, :application, :file, :function, :line, :module, :meta]
+#  config :logger, :console,
+#    format: {Timber.Formatter, :format},
+#    metadata: [:timber_context, :event, :application, :file, :function, :line, :module, :meta]
 
-  config :timber, Timber.Formatter,
-    colorize: true,
-    format: :logfmt,
-    print_timestamps: true,
-    print_log_level: true,
-    print_metadata: true # turn this on to view the additional metadata
+#  config :timber, Timber.Formatter,
+#    colorize: true,
+#    format: :logfmt,
+#    print_timestamps: true,
+#    print_log_level: false,
+#    print_metadata: false # turn this on to view the additional metadata
+else
+  # Update the instrumenters so that we can structure Phoenix logs
+  config :picape, PicapeWeb.Endpoint,
+         instrumenters: [Timber.Integrations.PhoenixInstrumenter]
+
+  # Structure Ecto logs
+  config :picape, Picape.Repo,
+         loggers: [{Timber.Integrations.EctoLogger, :log, []}]
+
+  # Use Timber as the logger backend
+  # Feel free to add additional backends if you want to send you logs to multiple devices.
+  # Deliver logs via HTTP to the Timber API by using the Timber HTTP backend.
+  config :logger,
+         backends: [Timber.LoggerBackends.HTTP],
+         utc_log: true
+
+  config :timber,
+         api_key: {:system, "TIMBER_LOGS_KEY"}
 end
 
 # Need help?
