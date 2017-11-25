fb41e902e13f504581dda3a4b557757dedf3b89f
Adrian Philipp <adrian@ticketswap.com>
2017-11-25 16:21:36 +0100

    Adds webpack-bundle-analyzer

--- assets/next.config.js
+++ assets/next.config.js
@@ -2,5 +2,16 @@ const Raven = require('raven');
 Raven.config(process.env.SENTRY_PUBLIC_DSN).install();
 
 module.exports = {
-    /* config options here */
+  webpack: function (config) {
+    if (process.env.ANALYZE) {
+        const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+        config.plugins.push(new BundleAnalyzerPlugin({
+            analyzerMode: 'server',
+            analyzerPort: 8888,
+            openAnalyzer: true
+        }))
+    }
+
+    return config
+  }
 }
