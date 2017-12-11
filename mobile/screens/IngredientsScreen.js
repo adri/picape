4b0eb31281361676b8fed7f27a580196d88f045a
Adrian Philipp <adrian@ticketswap.com>
2017-12-11 08:04:10 +0100

    Draft for ingredients list

--- mobile/screens/IngredientsScreen.js
+++ mobile/screens/IngredientsScreen.js
@@ -1,24 +1,57 @@
 import React from "react";
-import { ScrollView, StyleSheet } from "react-native";
+import { ActivityIndicator, ScrollView, StyleSheet, FlatList, Text, View, Image } from "react-native";
 import { ExpoLinksView } from "@expo/samples";
 import gql from "graphql-tag";
 import { graphql } from "react-apollo";
 
-class IngredientsScreenComponent extends React.Component {
-  static navigationOptions = {
-    title: "Ingredients",
-  };
+const toListData = ingredients =>
+  ingredients.edges.map(edge => ({
+    ...edge.node,
+    key: edge.node.id,
+  }));
 
-  render() {
-    return (
-      <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
-      </ScrollView>
-    );
+const IngredientsScreenComponent = function({ data: { loading, error, ingredients } }) {
+  console.log(ingredients && toListData(ingredients));
+  if (loading) {
+    return <ActivityIndicator />;
   }
-}
+
+  return (
+    <ScrollView style={styles.container}>
+      {error && (
+        <View>
+          <Text>Error! {error.message}</Text>
+        </View>
+      )}
+
+      {/*
+        * Wishlist:
+        * - Local search 🔍
+        * - Offline sync
+        *
+        **/}
+      {ingredients && (
+        <FlatList
+          contentContainerStyle={styles.listContainer}
+          style={styles.container}
+          keyExtractor={edge => edge.node.id}
+          data={ingredients.edges}
+          renderItem={({ item: { node } }) => (
+            <View style={styles.listItem}>
+              <Image source={{ url: node.imageUrl }} style={styles.image} />
+              <Text>{node.name}</Text>
+            </View>
+          )}
+        />
+      )}
+      <ExpoLinksView />
+    </ScrollView>
+  );
+};
+
+IngredientsScreenComponent.navigationOptions = {
+  title: "Ingredients",
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -26,9 +59,17 @@ const styles = StyleSheet.create({
     paddingTop: 15,
     backgroundColor: "#fff",
   },
+  image: {
+    width: 32,
+    height: 32,
+  },
+  listContainer: {},
+  listItem: {
+    flex: 1,
+  },
 });
 
-export const IngredientsScreen = graphql(gql`
+const ingredientsListQuery = gql`
   query IngredientList {
     ingredients(first: 1000, order: [{ field: NAME, direction: ASC }]) {
       edges {
@@ -43,4 +84,6 @@ export const IngredientsScreen = graphql(gql`
       }
     }
   }
-`)(IngredientsScreenComponent);
+`;
+
+export const IngredientsScreen = graphql(ingredientsListQuery)(IngredientsScreenComponent);
