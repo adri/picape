6195c90fe27eaffdbfd5e5a637c0afad247e2e6d
Adrian Philipp <adrian@ticketswap.com>
2018-03-07 15:06:20 +0100

    Fix required value

--- lib/picape_web/graphql/schema.ex
+++ lib/picape_web/graphql/schema.ex
@@ -109,7 +109,7 @@ defmodule PicapeWeb.Graphql.Schema do
     @desc "Order an ingredient in a certain quantity."
     field :order_ingredient, :ingredient do
       arg(:ingredient_id, non_null(:id))
-      arg(:quantity, non_null(:integer), default_value: 1)
+      arg(:quantity, :integer, default_value: 1)
       resolve(handle_errors(&Resolver.Order.order_ingredient/2))
     end
 
