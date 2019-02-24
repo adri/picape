d91c1cae301730e5df2a1811b4fb337b610cae06
Adrian Philipp <adrian@ticketswap.com>
2019-02-24 16:13:17 +0100

    Adds touch and launch images

--- assets/components/Layout.js
+++ assets/components/Layout.js
@@ -44,13 +44,66 @@ export default class Layout extends React.Component {
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
-          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <meta name="viewport" content="initial-scale=1.0, width=device-width, viewport-fit=cover" />
           <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
           <meta name="apple-mobile-web-app-title" content="Picape" />
 
           <title>{title}</title>
-          <link rel="apple-touch-icon" sizes="76x76" href="/images/apple-icon.png" />
-          <link rel="icon" type="image/png" href="/images/favicon.png" />
+
+          <link rel="apple-touch-icon" sizes="180x180" href="/static/images/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/static/images/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/static/images/favicon-16x16.png" />
+          <link
+            href="/static/images/iphone5_splash.png"
+            media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)"
+            rel="apple-touch-startup-image"
+          />
+          <link
+            href="/static/images/iphone6_splash.png"
+            media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)"
+            rel="apple-touch-startup-image"
+          />
+          <link
+            href="/static/images/iphoneplus_splash.png"
+            media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)"
+            rel="apple-touch-startup-image"
+          />
+          <link
+            href="/static/images/iphonex_splash.png"
+            media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)"
+            rel="apple-touch-startup-image"
+          />
+          <link
+            href="/static/images/iphonexr_splash.png"
+            media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)"
+            rel="apple-touch-startup-image"
+          />
+          <link
+            href="/static/images/iphonexsmax_splash.png"
+            media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)"
+            rel="apple-touch-startup-image"
+          />
+          <link
+            href="/static/images/ipad_splash.png"
+            media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)"
+            rel="apple-touch-startup-image"
+          />
+          <link
+            href="/static/images/ipadpro1_splash.png"
+            media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)"
+            rel="apple-touch-startup-image"
+          />
+          <link
+            href="/static/images/ipadpro3_splash.png"
+            media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)"
+            rel="apple-touch-startup-image"
+          />
+          <link
+            href="/static/images/ipadpro2_splash.png"
+            media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)"
+            rel="apple-touch-startup-image"
+          />
+
           <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
           <link href="/static/css/font-awesome.min.css" rel="stylesheet" />
           <link href="/static/css/bootstrap.min.css" rel="stylesheet" />
@@ -69,6 +122,12 @@ export default class Layout extends React.Component {
 
             body {
               background-color: transparent !important;
+            }
+
+            @media (min-width: 320px) and (max-width: 480px) {
+              body {
+                padding-top: 30px;
+              }
             }
 
             body > div:first-child,
