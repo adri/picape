7d0a76e13e1d3479404e2f5cfc6a6408ab0a7c15
Adrian Philipp <adrian@ticketswap.com>
2024-05-11 15:27:08 +0200

    Fix redirect

--- lib/picape_web/controllers/page_controller.ex
+++ lib/picape_web/controllers/page_controller.ex
@@ -5,7 +5,7 @@ defmodule PicapeWeb.PageController do
 
   def index(conn, _params) do
     conn
-    |> put_resp_header("location", "//index.html")
+    |> put_resp_header("location", "/index.html")
     |> send_resp(301, "index.html")
   end
 end
